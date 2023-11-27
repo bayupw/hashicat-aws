25e20b563ca38f468a5f99230a6c14e801dcec41
Bayu Wibowo <bayupw@gmail.com>
2023-11-27 18:54:58 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,10 @@ module "s3-bucket" {
   source  = "app.terraform.io/bayu-terraform-training/s3-bucket/aws"
   version = "2.8.0"
 
-  acl    = "private"
   bucket_prefix = var.prefix
+
+  tags = {
+    name = "${var.prefix}-vpc-${var.region}"
+    environment = "Production"
+  }
 }
