8cefe181b56173269adac0911227f4948938e389
Bayu Wibowo <bayupw@gmail.com>
2023-11-27 18:35:55 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Departner = "devops"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
